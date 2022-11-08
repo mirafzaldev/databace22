7cdba7e3ba9b5661f44383b3a3794d6be6cb27e5
backendproj <susanfas98@gmail.com>
2022-11-08 10:12:08 +0500

    mirafzal ni fazifasi

---
+++ code MIrafzal.sql
@@ -0,0 +1,79 @@
+CREATE TABLE school(
+  student_id INT,
+  student_name VARCHAR(255),
+  grade INT,
+  univercity TEXT
+);
+
+
+INSERT INTO school VALUES(
+  '1',
+  'Baxodirov Mirafzal',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '2',
+  'Akbaraliyev Xumoyun',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '3',
+  'Azamat Abduraimov',
+  '5',
+  'MGIMO'
+);
+
+INSERT INTO school VALUES(
+  '4',
+  'Orazali Abdugapparov',
+  '5',
+  'MISIS'
+);
+
+INSERT INTO school VALUES(
+  '5',
+  'Diyorbek Abduqaimov',
+  '5',
+  'MISIS'
+);
+
+INSERT INTO school VALUES(
+  '6',
+  'Asadbek Ergashev',
+  '5',
+  'MISIS'
+);
+
+INSERT INTO school VALUES(
+  '7',
+  'Axmatov Alimardon',
+  '5',
+  'Moliya'
+);
+
+INSERT INTO school VALUES(
+  '8',
+  'Abdirimov Siyovush',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '9',
+  'Fazliddin Mirzayev',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '10',
+  'Abdumanobov Nodir',
+  '5',
+  'IRrigsatsiya'
+);
+
+SELECT * FROM school;
